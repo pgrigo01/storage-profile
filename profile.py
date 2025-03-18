550f678035b2a87035f489f7204f3b16a44c13b4
Panagiotis Grigoriou <90858207+pgrigo01@users.noreply.github.com>
2025-03-18 17:14:17 +0000

    modified profile.py

--- profile.py
+++ profile.py
@@ -11,7 +11,8 @@ import geni.rspec.emulab as emulab
 pc = portal.Context()
 
 # ---------------------------------------------------------------------------
-# Define Aggregates and Images Options
+# Aggregates and Images
+
 agglist = [
     ("urn:publicid:IDN+emulab.net+authority+cm", "emulab.net"),
     ("urn:publicid:IDN+utah.cloudlab.us+authority+cm", "utah.cloudlab.us"),
@@ -29,194 +30,93 @@ imagelist = [
     ('urn:publicid:IDN+emulab.net+image+emulab-ops//FBSD113-64-STD', 'FreeBSD 11.3')
 ]
 
-# ---------------------------------------------------------------------------
-# Existing VM and Shared VLAN Parameters
-
 pc.defineParameter(
-    "aggregate", "Specific Aggregate",
+    "aggregate", "Select Aggregate (Cluster)",
     portal.ParameterType.STRING,
-    agglist[0][0], agglist)
+    agglist[0][0], 
+    agglist,
+    longDescription="Pick which cluster you want to run on."
+)
 
 pc.defineParameter(
     "image", "Node Image",
     portal.ParameterType.IMAGE,
     imagelist[0][0],
     imagelist,
-    longDescription="The image your node will run.")
+    longDescription="Which OS image the RawPC will use."
+)
 
 pc.defineParameter(
     "routableIP", "Routable IP",
-    portal.ParameterType.BOOLEAN, False,
-    longDescription="Add a routable IP to the VM.")
-
-pc.defineStructParameter(
-    "sharedVlans", "Add Shared VLAN", [],
-    multiValue=True, itemDefaultValue={}, min=0, max=None,
-    members=[
-        portal.Parameter(
-            "createSharedVlan", "Create Shared VLAN",
-            portal.ParameterType.BOOLEAN, False,
-            longDescription="Create a new shared VLAN with the name above, and connect the first node to it."),
-        portal.Parameter(
-            "connectSharedVlan", "Connect to Shared VLAN",
-            portal.ParameterType.BOOLEAN, False,
-            longDescription="Connect an existing shared VLAN with the name below to the first node."),
-        portal.Parameter(
-            "name", "Shared VLAN Name",
-            portal.ParameterType.STRING, "",
-            longDescription="A shared VLAN name (functions as a private key allowing other experiments to connect to this node/VLAN), used when the 'Create Shared VLAN' or 'Connect to Shared VLAN' options above are selected. Must be fewer than 32 alphanumeric characters."),
-        portal.Parameter(
-            "ip_address", "Shared VLAN IP Address",
-            portal.ParameterType.STRING, "10.254.254.1",
-            longDescription="Set the IP address for the shared VLAN interface. Make sure to use an unused address within the subnet of an existing shared vlan!"),
-        portal.Parameter(
-            "subnet_mask", "Shared VLAN Netmask",
-            portal.ParameterType.STRING, "255.255.255.0",
-            longDescription="Set the subnet mask for the shared VLAN interface, as a dotted quad.")
-    ])
+    portal.ParameterType.BOOLEAN, 
+    False,
+    longDescription="Add a routable IP to the node."
+)
 
 # ---------------------------------------------------------------------------
-# New Parameters for Short-Term Dataset Integration
+# Short-Term (Ephemeral) Local Blockstore Parameters
 
 pc.defineParameter(
     "wantShortTermDataset",
-    "Create a Short-Term Dataset?",
+    "Create an Ephemeral Local Blockstore?",
     portal.ParameterType.BOOLEAN,
-    False,
-    longDescription="Select this option if you want to attach a short-term (ephemeral) dataset to your node."
+    True,
+    longDescription="Select this option to create ephemeral 'scratch' space on the local disk."
 )
 
-# (Note: For ephemeral datasets, do not specify a dataset name,
-# since setting a dataset attribute forces persistent behavior with URN validation.)
 pc.defineParameter(
     "shortTermDatasetSizeGB",
-    "Short-Term Dataset Size (GB)",
+    "Ephemeral Blockstore Size (GB)",
     portal.ParameterType.INTEGER,
     10,
-    longDescription="The size of the short-term dataset in gigabytes."
+    longDescription="Size of the local ephemeral blockstore in gigabytes."
 )
 
 pc.defineParameter(
     "shortTermDatasetMountPoint",
-    "Mount Point for Short-Term Dataset",
+    "Mount Point for Ephemeral Blockstore",
     portal.ParameterType.STRING,
     "/mydata",
-    longDescription="The mount point where the short-term dataset will be attached to the node."
+    longDescription="The mount point for the ephemeral local disk on your node."
 )
-
-# Optional: Specify a placement cluster for the dataset.
-datasetClusters = [
-    ("ANY", "Any Available Cluster"),
-    ("utah", "Utah"),
-    ("wisc", "Wisconsin"),
-    ("clemson", "Clemson")
-]
-pc.defineParameter(
-    "shortTermDatasetPlacement",
-    "Dataset Placement (Cluster)",
-    portal.ParameterType.STRING,
-    "ANY",
-    datasetClusters,
-    longDescription="Select the cluster for the short-term dataset, or 'Any' for automatic placement."
-)
-
-# ---------------------------------------------------------------------------
-# Bind Parameters and Validate Shared VLAN Options
 
 params = pc.bindParameters()
 
-i = 0
-for x in params.sharedVlans:
-    n = 0
-    if x.createSharedVlan:
-        n += 1
-    if x.connectSharedVlan:
-        n += 1
-    if n > 1:
-        err = portal.ParameterError(
-            "Must choose only a single shared VLAN operation (create, connect)",
-            ['sharedVlans[%d].createSharedVlan' % (i,),
-             'sharedVlans[%d].connectSharedVlan' % (i,)])
-        pc.reportError(err)
-    if n == 0:
-        err = portal.ParameterError(
-            "Must choose one of the shared VLAN operations: create, connect",
-            ['sharedVlans[%d].createSharedVlan' % (i,),
-             'sharedVlans[%d].connectSharedVlan' % (i,)])
-        pc.reportError(err)
-    i += 1
-
+# Validate parameters, if needed.
 pc.verifyParameters()
 
 # ---------------------------------------------------------------------------
-# Create the RSpec Request Object
-
+# Create the RSpec Request
 request = pc.makeRequestRSpec()
 
-# Optional Tour Description
+# Optional: Add a Tour Description
 tour = ig.Tour()
-tour.Description(ig.Tour.TEXT, "Create a shared-mode VM with optional short-term dataset and shared VLAN(s).")
+tour.Description(ig.Tour.TEXT, "Create a single RawPC node with ephemeral local blockstore.")
 request.addTour(tour)
 
 # ---------------------------------------------------------------------------
-# Create the VM Node
+# Create a RawPC node (not XenVM), so local ephemeral storage is possible
+node = request.RawPC("node-0")
 
-node = ig.XenVM("node-0")
+# Set the OS image
 node.disk_image = params.image
-node.exclusive = False
 
+# If you want a routable IP
 if params.routableIP:
     node.routable_control_ip = True
 
+# Use aggregator param to pick the cluster
 if params.aggregate:
     node.component_manager_id = params.aggregate
 
-if params.image:
-    node.disk_image = params.image
-
-# Process any Shared VLANs defined by the user
-sharedvlans = []
-k = 0
-for x in params.sharedVlans:
-    iface = node.addInterface("ifSharedVlan%d" % (k,))
-    if x.ip_address:
-        iface.addAddress(pg.IPv4Address(x.ip_address, x.subnet_mask))
-    sharedvlan = pg.Link('shared-vlan-%d' % (k,))
-    sharedvlan.addInterface(iface)
-    if x.createSharedVlan:
-        sharedvlan.createSharedVlan(x.name)
-    else:
-        sharedvlan.connectSharedVlan(x.name)
-    sharedvlan.link_multiplexing = True
-    sharedvlan.best_effort = True
-    sharedvlans.append(sharedvlan)
-    k += 1
-
-# Add the node and shared VLAN resources to the request
-request.addResource(node)
-for sv in sharedvlans:
-    request.addResource(sv)
-
 # ---------------------------------------------------------------------------
-# Integrate the Short-Term (Ephemeral) Dataset if Requested
+# Ephemeral Local Blockstore (Scratch Disk) if desired
 if params.wantShortTermDataset:
-    # Create a RemoteBlockstore resource for the short-term dataset.
-    shortTermBS = request.RemoteBlockstore("shortTermDS", params.shortTermDatasetMountPoint)
-    
-    # Do not set 'dataset' here; that attribute is for persistent datasets (which require a valid URN).
-    shortTermBS.temporary = True  # Marks this as a short-term (ephemeral) dataset.
-    shortTermBS.size = "{}GB".format(params.shortTermDatasetSizeGB)
-    
-    # Optionally, enforce placement on a specific cluster if not set to "ANY".
-    if params.shortTermDatasetPlacement != "ANY":
-        shortTermBS.placement = params.shortTermDatasetPlacement
-
-    # Optionally, add a service to create the mount directory on the node.
-    node.addService(pg.Execute(
-        shell="sh",
-        command="sudo mkdir -p {}".format(params.shortTermDatasetMountPoint)
-    ))
+    bs = request.Blockstore("bs0", params.shortTermDatasetMountPoint)
+    bs.size = "{}GB".format(params.shortTermDatasetSizeGB)
+    bs.temporary = True  # ephemeral
+    bs.mount = node  # attach to the node
 
 # ---------------------------------------------------------------------------
-# Print the final RSpec
+# Print the RSpec
 pc.printRequestRSpec(request)
